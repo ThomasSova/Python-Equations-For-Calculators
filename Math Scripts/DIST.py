ccc91959ce8393095a3c4483c1fa9dd3d8338375
ThomasSova <78554772+ThomasSova@users.noreply.github.com>
2022-04-12 10:36:57 -0400

    Create DIST.py

---
+++ Math Scripts/DIST.py
@@ -0,0 +1,23 @@
+# Math Calculations
+from math import *
+
+print("X1 = ")
+x1 = float(input())
+
+print("Y1 = ")
+y1 = float(input())
+
+print("X2 = ")
+x2 = float(input())
+
+print("Y2 = ")
+y2 = float(input())
+
+print("(",x1,",",y1,")")
+print("(",x2,",",y2,")")
+
+a=(x2-x1)**2+(y2-y1)**2
+
+d=sqrt(a)
+
+print(d)

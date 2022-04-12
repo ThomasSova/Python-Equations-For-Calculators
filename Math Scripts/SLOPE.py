02bdae6ea2bd953794e8ab261053d5716ad90ea8
ThomasSova <78554772+ThomasSova@users.noreply.github.com>
2022-04-12 10:51:56 -0400

    Create SLOPE.py

---
+++ Math Scripts/SLOPE.py
@@ -0,0 +1,25 @@
+# Math Calculations
+from math import *
+
+print("X1 = ")
+x1=float(input())
+
+print("Y1 = ")
+y1=float(input())
+
+print("X2 = ")
+x2=float(input())
+
+print("Y2 = ")
+y2=float(input())
+
+
+print("(",x1,",",y1,")")
+print("(",x2,",",y2,")")
+
+a = y2-y1
+b = x2-x1
+
+m=a/b
+
+print("slope = ",m)
